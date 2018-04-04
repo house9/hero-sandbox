3df2ecfc6774e4cdce4b1521f0e7b2b210190a34
Jesse House <mail@jessehouse.com>
2018-04-04 16:56:02 -0700

    fix release script

--- config/deploy/heroku_release_steps.sh
+++ config/deploy/heroku_release_steps.sh
@@ -1,8 +1,10 @@
-# if [[ $HEROKU_APP_NAME =~ (staging|production)$ ]]; then
-#   echo 'exit on error.....'
-#   set -e # exit on error
-# fi
+# fail release on staging and production
+# workaround for review apps
+echo '------------------------------'
+echo $HEROKU_APP_NAME
+echo '------------------------------'
 
+if [[ $HEROKU_APP_NAME =~ (staging|production)$ ]]; then echo 'exit on error.....'; set -e; fi
 
 echo '============================='
 echo 'START: custom release steps'
@@ -12,11 +14,11 @@ echo 'START: custom release steps'
 # bundle exec rake assets_nodigest
 # echo '-----------------------------'
 
-# echo '-----------------------------'
-# echo 'RELEASE STEP: db:migrate'
-# echo 'NOTE: this will fail 1st time on Preview Apps'
-# bundle exec rake db:migrate
-# echo '-----------------------------'
+echo '-----------------------------'
+echo 'RELEASE STEP: db:migrate'
+echo 'NOTE: this will fail 1st time on Preview Apps'
+bundle exec rake db:migrate
+echo '-----------------------------'
 
 echo 'END: custom release steps'
 echo '============================='
--- config/deploy/heroku_review_app_postdeploy.sh
+++ config/deploy/heroku_review_app_postdeploy.sh
@@ -5,6 +5,7 @@ echo 'START: review app postdeploy'
 
 # NOTE: this script runs 1 time when heroku creates review app, not every deploy
 # it runs after Profile#release step
+
 bundle exec rake db:structure:load
 bundle exec rake db:seed
 
