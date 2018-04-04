c0facde8781c2f08fe451fae5341bb7e42d74b69
Jesse House <mail@jessehouse.com>
2018-04-04 14:48:28 -0700

    run seed when deploying review app

--- config/deploy/heroku_review_app.sh
+++ config/deploy/heroku_review_app.sh
@@ -3,7 +3,9 @@ set -e # exit on error
 echo '============================='
 echo 'START: review app postdeploy'
 
+# NOTE: this script runs 1 time when heroku creates review app, not every deploy
 bundle exec rake db:structure:load
+bundle exec rake db:seed
 
 echo 'END: review app postdeploy'
 echo '============================='
