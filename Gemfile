a6345565c18579127ca7515e3003f7a0395b543b
Jesse House <mail@jessehouse.com>
2018-04-04 17:41:47 -0700

    add rubocop

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 source 'https://rubygems.org'
 
 gem 'rails', '4.2.10'
@@ -13,14 +15,11 @@ gem 'turbolinks'
 gem 'uglifier', '>= 1.3.0'
 
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'rubocop'
 end
 
 group :development do
-  # Access an IRB console on exception pages or by using <%= console %> in views
-  gem 'web-console', '~> 2.0'
-
-  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  gem 'web-console', '~> 2.0'
 end
--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class WidgetsController < ApplicationController
-  before_action :set_widget, only: [:show, :edit, :update, :destroy]
+  before_action :set_widget, only: %i[show edit update destroy]
 
   # GET /widgets
   # GET /widgets.json
@@ -9,8 +11,7 @@ class WidgetsController < ApplicationController
 
   # GET /widgets/1
   # GET /widgets/1.json
-  def show
-  end
+  def show; end
 
   # GET /widgets/new
   def new
@@ -18,8 +19,7 @@ class WidgetsController < ApplicationController
   end
 
   # GET /widgets/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /widgets
   # POST /widgets.json
@@ -62,13 +62,14 @@ class WidgetsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_widget
-      @widget = Widget.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def widget_params
-      params.require(:widget).permit(:name, :status, :category)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_widget
+    @widget = Widget.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def widget_params
+    params.require(:widget).permit(:name, :status, :category)
+  end
 end
